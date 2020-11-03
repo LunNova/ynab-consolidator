d765091e1e672896007209fb79f5b376d8ba7838
Luna <git@nyx.nova.fail>
2020-11-03 16:33:26 +0000

    More logging for which accounts were used, skip closed accounts

--- app.py
+++ app.py
@@ -10,10 +10,10 @@ from ynab import AccountsApi, BudgetDetailResponse, CategoriesApi, TransactionsA
 from ynab import ApiClient, BudgetsApi, MonthsApi, Account, BudgetDetail
 
 
-def make_prefix(budget_name):
-    parts = budget_name.split()
+def make_prefix(budget_name: str):
+    parts = budget_name.strip().split()
     if len(parts) == 1:
-        return budget_name
+        return f"{budget_name} "
     return f"{''.join([word[0] for word in parts])} "
 
 
@@ -52,6 +52,8 @@ class Client:
             prefix = make_prefix(budget_name)
             print(f"{budget_name} {currency}")
             for acc in accs:
+                if acc["closed"]:
+                    continue
                 name = acc["name"]
                 if not prefix in name:
                     name = prefix + name
@@ -64,27 +66,39 @@ class Client:
         category_id = categories_api.get_categories(dest_budget).data[
             "category_groups"
         ][0]["categories"][0]["id"]
+
+        done_accs: List[str] = []
+
         accs: List[Account] = accounts_api.get_accounts(dest_budget).data["accounts"]
         for acc in accs:
             name = acc["name"]
             balance = acc["balance"]
-            if name.lower() in converted:
-                target_bal, orig_bal, currency, rate = converted[name.lower()]
-                if int(target_bal) == int(balance):
-                    continue
-                print(f"Updating {name} to {target_bal}")
-                tran = {
-                    "account_id": acc["id"],
-                    "date": today.isoformat(),
-                    "amount": target_bal - balance,
-                    "memo": f"{orig_bal/1000} {currency} @ {round(rate, 3)}",
-                    "approved": True,
-                    "payee_id": None,
-                    "payee_name": None,
-                    "category_id": category_id,
-                    "cleared": "cleared",
-                }
-                transactions_api.create_transaction({"transaction": tran}, dest_budget)
+            if name.lower() not in converted:
+                print(f"Can't find budget {name}, skipping")
+                continue
+
+            done_accs.append(name.lower())
+            target_bal, orig_bal, currency, rate = converted[name.lower()]
+            if int(target_bal) == int(balance):
+                continue
+            print(f"Updating {name} to {target_bal}")
+            tran = {
+                "account_id": acc["id"],
+                "date": today.isoformat(),
+                "amount": target_bal - balance,
+                "memo": f"{orig_bal/1000} {currency} @ {round(rate, 3)}",
+                "approved": True,
+                "payee_id": None,
+                "payee_name": None,
+                "category_id": category_id,
+                "cleared": "cleared",
+            }
+            transactions_api.create_transaction({"transaction": tran}, dest_budget)
+
+        missing_accs_in_dest = [key for key in converted.keys() if key not in done_accs]
+        if len(missing_accs_in_dest) > 0:
+            missing_accs_output = "', '".join(missing_accs_in_dest)
+            print(f"Missing accounts: '{missing_accs_output}'")
 
 
 if __name__ == "__main__":
